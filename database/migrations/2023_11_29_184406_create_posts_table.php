49c5ff6c1956f06ae8c6cbbd6835ca3e9b84c071
Jareer Zeenam <jareerzeenam.28@gmail.com>
2024-05-28 17:45:32 +0100

    Comments and Posts elements created

---
+++ database/migrations/2023_11_29_184406_create_posts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained()->restrictOnDelete(); // This will cause a DB exception if the user who has posts was deleted
+            $table->string('title');
+            $table->longText('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
