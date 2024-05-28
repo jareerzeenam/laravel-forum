11a4fa2cc79833484f53f2abd790a9a9c17e7aa1
Jareer Zeenam <jareerzeenam.28@gmail.com>
2024-05-28 19:47:23 +0100

    Database Factories and Seeders added

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,9 @@ class CommentFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'post_id' => Post::factory(),
+            'body' => $this->faker->realText(250),
         ];
     }
 }
