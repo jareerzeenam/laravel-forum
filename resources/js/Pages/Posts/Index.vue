13d19c90b0b8c77f5977eb38b7f544408dbbbc02
Jareer Zeenam <jareerzeenam.28@gmail.com>
2024-06-02 00:10:33 +0100

    Posts linking up the indexes completed

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -2,8 +2,11 @@
     <AppLayout>
         <Container>
         <ul class="divide-y">
-            <li v-for="post in posts.data" :key="post.id" class="px-2 py-4">
-                <span class="font-bold text-lg">{{ post.title }}</span>
+            <li v-for="post in posts.data" :key="post.id">
+                <Link :href="route('posts.show', post.id)" class="block group px-2 py-4">
+                    <span class="font-bold text-lg group-hover:text-indigo-600">{{ post.title }}</span>
+                    <span class="block pt-1 text-sm text-gray-600">{{ formattedDate(post) }} by {{ post.user.name }}</span>
+                </Link>
             </li>
         </ul>
 
@@ -16,6 +19,13 @@
 import AppLayout from "@/Layouts/AppLayout.vue";
 import Container from "@/Components/Container.vue";
 import Pagination from "@/Components/Pagination.vue";
+import { Link } from '@inertiajs/vue3'
+import moment from "moment";
 
-defineProps(['posts'])
+defineProps(['posts']);
+
+
+const formattedDate = (post) => {
+    return moment(post.created_at).fromNow();
+}
 </script>
