72edc65ca08e410d2b7cc07279c856a1ca577c59
Jareer Zeenam <jareerzeenam.28@gmail.com>
2024-06-01 22:48:26 +0100

    Show Post and Unit testing completed

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -4,6 +4,9 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Collection;
+use function fake;
+use const PHP_EOL;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -19,8 +22,8 @@ class PostFactory extends Factory
     {
         return [
             'user_id' => User::factory(),
-            'title' => str($this->faker->sentence)->beforeLast('.')->title(),
-            'body' => $this->faker->realText(600),
+            'title' => str(fake()->sentence)->beforeLast('.')->title(),
+            'body' => Collection::times(4, fn() => fake()->realText(1250))->join(PHP_EOL . PHP_EOL),
         ];
     }
 }
---
+++ resources/js/Pages/Posts/Show.vue
@@ -0,0 +1,24 @@
+<template>
+    <AppLayout :title="post.title">
+        <Container>
+            <Heading :title="post.title"/>
+            <span class="block mt-1 text-sm text-gray-600">{{ formattedDate }} by {{ post.user.name }}</span>
+
+            <article class="mt-6">
+                <pre class="whitespace-pre-wrap font-sans">{{ post.body }}</pre>
+            </article>
+        </Container>
+    </AppLayout>
+</template>
+
+<script setup>
+import AppLayout from "@/Layouts/AppLayout.vue";
+import {computed} from "vue";
+import moment from "moment";
+import Container from "@/Components/Container.vue";
+import Heading from "@/Components/Heading.vue";
+
+const props = defineProps(['post']);
+
+const formattedDate = computed(()=> moment(props.post.created_at).fromNow());
+</script>
