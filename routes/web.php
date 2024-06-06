6ad49e79ef60b6493880ab853d6a75e3caabf6cd
Jareer Zeenam <jareerzeenam.28@gmail.com>
2024-06-07 00:36:41 +0100

    Post Comment Backend and Unit test completed

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Resources\CommentResource;
 use App\Http\Resources\PostResource;
 use App\Http\Resources\UserResource;
@@ -43,3 +44,6 @@ Route::middleware([
 
 Route::get('posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
+
+Route::post('posts/{post}/comments', [CommentController::class, 'store'])->name('posts.comments.store');
