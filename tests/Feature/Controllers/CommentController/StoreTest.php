1f32beff2fe5ce7d98fdcadbcbbf7304356ce91c
Jareer Zeenam <jareerzeenam.28@gmail.com>
2024-06-07 01:39:53 +0100

    Post Comment Auth issue fixed and added unit test

--- tests/Feature/Controllers/CommentController/StoreTest.php
+++ tests/Feature/Controllers/CommentController/StoreTest.php
@@ -4,6 +4,12 @@ use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
 use function Pest\Laravel\actingAs;
+use function Pest\Laravel\post;
+
+it('requires authentication', function () {
+    post(route('posts.comments.store', Post::factory()->create()))
+        ->assertRedirect(route('login'));
+});
 
 it('can store a comment', function () {
     $user = User::factory()->create();
