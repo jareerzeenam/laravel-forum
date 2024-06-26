e633ccb6f2768ee211c469bffa7806ed3dcf20d6
Jareer Zeenam <jareerzeenam.28@gmail.com>
2024-06-26 02:31:44 +0100

    Resolved failing Testcases

--- tests/Feature/Controllers/PostController/ShowTest.php
+++ tests/Feature/Controllers/PostController/ShowTest.php
@@ -10,7 +10,7 @@ use function Pest\Laravel\get;
 it('can show a post', function () {
     $post = Post::factory()->create();
 
-    get(route('posts.show', $post))
+    get($post->showRoute())
         ->assertComponent('Posts/Show');
 });
 
@@ -18,7 +18,7 @@ it('passes a post to the view', function () {
     $post = Post::factory()->create();
     $post->load('user');
 
-    get(route('posts.show', $post))
+    get($post->showRoute())
         ->assertHasResource('post', PostResource::make($post));
 });
 
@@ -28,6 +28,6 @@ it('passes comments to the view', function () {
     $comments = Comment::factory(2)->for($post)->create();
     $comments->load('user');
 
-    get(route('posts.show', $post))
+    get($post->showRoute())
         ->assertHasPaginatedResource('comments', CommentResource::collection($comments->reverse()));
 });
