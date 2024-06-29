8942ef1d682af746a98e17d1408d0b29efac711e
Jareer Zeenam <jareerzeenam.28@gmail.com>
2024-06-29 01:47:57 +0100

    Markdown Toolbar added

--- resources/js/Components/MarkdownEditor.vue
+++ resources/js/Components/MarkdownEditor.vue
@@ -1,5 +1,116 @@
 <template>
-    <div class="bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600">
+    <div v-if="editor" class="bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600">
+        <menu class="flex divide-x border-b">
+            <li>
+                <button @click="() => editor.chain().focus().toggleBold().run()"
+                        type="button"
+                        class="px-3 py-2 rounded-tl-md"
+                        :class="[editor.isActive('bold') ? 'bg-indigo-500 text-white' : 'hover:bg-gray-200',]"
+                        title="Bold">
+                    <i class="ri-bold"></i>
+                </button>
+            </li>
+            <li>
+                <button @click="() => editor.chain().focus().toggleItalic().run()"
+                        type="button"
+                        class="px-3 py-2"
+                        :class="[editor.isActive('italic') ? 'bg-indigo-500 text-white' : 'hover:bg-gray-200',]"
+                        title="Italic">
+                    <i class="ri-italic"></i>
+                </button>
+            </li>
+            <li>
+                <button @click="() => editor.chain().focus().toggleStrike().run()"
+                        type="button"
+                        class="px-3 py-2"
+                        :class="[editor.isActive('strike') ? 'bg-indigo-500 text-white' : 'hover:bg-gray-200',]"
+                        title="Strike">
+                    <i class="ri-strikethrough"></i>
+                </button>
+            </li>
+            <li>
+                <button @click="() => editor.chain().focus().toggleBlockquote().run()"
+                        type="button"
+                        class="px-3 py-2"
+                        :class="[editor.isActive('blockquote') ? 'bg-indigo-500 text-white' : 'hover:bg-gray-200',]"
+                        title="Blockquote">
+                    <i class="ri-double-quotes-l"></i>
+                </button>
+            </li>
+            <li>
+                <button @click="() => editor.chain().focus().toggleBulletList().run()"
+                        type="button"
+                        class="px-3 py-2"
+                        :class="[editor.isActive('bulletList') ? 'bg-indigo-500 text-white' : 'hover:bg-gray-200',]"
+                        title="Bullet List">
+                    <i class="ri-list-unordered"></i>
+                </button>
+            </li>
+            <li>
+                <button @click="() => editor.chain().focus().toggleOrderedList().run()"
+                        type="button"
+                        class="px-3 py-2"
+                        :class="[editor.isActive('orderedList') ? 'bg-indigo-500 text-white' : 'hover:bg-gray-200',]"
+                        title="Ordered List">
+                    <i class="ri-list-ordered"></i>
+                </button>
+            </li>
+            <li>
+                <button @click="promptUserForHref"
+                        type="button"
+                        class="px-3 py-2"
+                        :class="[editor.isActive('link') ? 'bg-indigo-500 text-white' : 'hover:bg-gray-200',]"
+                        title="Add Link">
+                    <i class="ri-link"></i>
+                </button>
+            </li>
+            <li>
+                <button @click="() => editor.chain().focus().toggleHeading({ level:2 }).run()"
+                        type="button"
+                        class="px-3 py-2"
+                        :class="[editor.isActive('heading',{ level:2 }) ? 'bg-indigo-500 text-white' : 'hover:bg-gray-200',]"
+                        title="Heading 1">
+                    <i class="ri-h-1"></i>
+                </button>
+            </li>
+            <li>
+                <button @click="() => editor.chain().focus().toggleHeading({ level:3 }).run()"
+                        type="button"
+                        class="px-3 py-2"
+                        :class="[editor.isActive('heading',{ level:3 }) ? 'bg-indigo-500 text-white' : 'hover:bg-gray-200',]"
+                        title="Heading 2">
+                    <i class="ri-h-2"></i>
+                </button>
+            </li>
+            <li>
+                <button @click="() => editor.chain().focus().toggleHeading({ level:4 }).run()"
+                        type="button"
+                        class="px-3 py-2"
+                        :class="[editor.isActive('heading',{ level:4 }) ? 'bg-indigo-500 text-white' : 'hover:bg-gray-200',]"
+                        title="Heading 3">
+                    <i class="ri-h-3"></i>
+                </button>
+            </li>
+
+            <li>
+                <button @click="() => editor.chain().focus().toggleCode().run()"
+                        type="button"
+                        class="px-3 py-2"
+                        :class="[editor.isActive('code') ? 'bg-indigo-500 text-white' : 'hover:bg-gray-200',]"
+                        title="Code">
+                    <i class="ri-code-view"></i>
+                </button>
+            </li>
+            <li>
+                <button @click="() => editor.chain().focus().toggleCodeBlock().run()"
+                        type="button"
+                        class="px-3 py-2"
+                        :class="[editor.isActive('codeBlock') ? 'bg-indigo-500 text-white' : 'hover:bg-gray-200',]"
+                        title="Code Block">
+                    <i class="ri-code-block"></i>
+                </button>
+            </li>
+        </menu>
         <EditorContent :editor="editor" />
     </div>
 </template>
@@ -9,6 +120,8 @@ import {EditorContent, useEditor} from "@tiptap/vue-3";
 import {StarterKit} from "@tiptap/starter-kit";
 import {watch} from "vue";
 import {Markdown} from "tiptap-markdown";
+import 'remixicon/fonts/remixicon.css';
+import Link from '@tiptap/extension-link';
 
 const props = defineProps({
     modelValue:'',
@@ -19,8 +132,13 @@ const emit = defineEmits(['update:modelValue']);
 
 const editor = useEditor({
     extensions: [
-        StarterKit,
+        StarterKit.configure({
+            heading: {
+                levels: [2, 3, 4],
+            },
+        }),
         Markdown,
+        Link,
     ],
     editorProps: {
         attributes: {
@@ -38,4 +156,19 @@ watch(() => props.modelValue,(value) => {
 },{
     immediate:true,
 });
+
+const promptUserForHref = () => {
+    if (editor.value?.isActive('link')) {
+        return editor.value?.chain().focus().unsetLink().run();
+    }
+
+    const href = prompt('Where do you want to link to?');
+    if (!href) {
+     return editor.value?.chain().focus().run();
+    }
+
+    return editor.value?.chain().focus().setLink({ href }).run();
+
+};
+
 </script>
