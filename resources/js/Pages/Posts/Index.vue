a2ceb5b83c51e3d3bbc26c6cc16fbfbe7f0dae80
Jareer Zeenam <jareerzeenam.28@gmail.com>
2024-07-02 02:06:07 +0100

    Post Topic category initialized

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -2,10 +2,14 @@
     <AppLayout>
         <Container>
         <ul class="divide-y">
-            <li v-for="post in posts.data" :key="post.id">
+            <li v-for="post in posts.data" :key="post.id" class="flex justify-between items-baseline flex-col md:flex-row">
                 <Link :href="post.routes.show" class="block group px-2 py-4">
                     <span class="first-letter:uppercase font-bold text-lg group-hover:text-indigo-600">{{ post.title }}</span>
                     <span class="block pt-1 text-sm text-gray-600">{{ formattedDate(post) }} by {{ post.user.name }}</span>
+                </Link>
+
+                <Link href="#!" class="mb-2 rounded-full py-0.5 px-2 border border-pink-500 text-pink-500 hover:bg-indigo-500 hover:text-white hover:border-none">
+                    {{ post.topic.name}}
                 </Link>
             </li>
         </ul>
