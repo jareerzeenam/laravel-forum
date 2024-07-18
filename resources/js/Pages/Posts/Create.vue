7045e7d0bd58366679fc7bdae493cffbe986e134
Jareer Zeenam <jareerzeenam.28@gmail.com>
2024-07-18 02:00:57 +0100

    Vue Upgraded and component feature improvements

--- resources/js/Pages/Posts/Create.vue
+++ resources/js/Pages/Posts/Create.vue
@@ -64,7 +64,7 @@ const props = defineProps(['topics']);
 const form = useForm({
     title: '',
     topic_id: props.topics[0].id,
-    body: 'Hello world!',
+    body: '',
 });
 
 const createPost = () => form.post(route('posts.store'));
